bb4b26b10404c1e77d9153494e1ec24c609ebc32
Kasper Timm Hansen <hey@kaspth.com>
2026-01-08 09:41:26 +0100

    disable fixtures

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,9 @@ module Peak
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
 
+    # We prefer Oaken to fixtures, so we disable them here.
+    config.app_generators { _1.test_framework _1.test_framework, fixture: false }
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files

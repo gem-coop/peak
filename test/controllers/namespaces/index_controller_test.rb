80872856131d49b3047f58281931f92271b0a211
Kasper Timm Hansen <hey@kaspth.com>
2026-01-09 09:27:50 +0100

    setup empty bun so ci can run

--- test/controllers/namespaces/index_controller_test.rb
+++ test/controllers/namespaces/index_controller_test.rb
@@ -1,13 +1,4 @@
 require "test_helper"
 
 class Namespaces::IndexControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get namespaces_index_index_url
-    assert_response :success
-  end
-
-  test "should get show" do
-    get namespaces_index_show_url
-    assert_response :success
-  end
 end

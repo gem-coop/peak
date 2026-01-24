38851c57d6cc10b56ea1cae5990e7fecb81b6df4
André Arko <andre@arko.net>
2026-01-23 23:47:55 -0800

    take @kaspth's suggestion of 10.minutes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,7 +53,7 @@ Rails.application.configure do
   config.active_job.queue_adapter = ActiveJob::QueueAdapters::AsyncAdapter.new(
     min_threads: 1,
     max_threads: 2 * Concurrent.processor_count,
-    idletime: 600.seconds
+    idletime: 10.minutes
   )
 
   # Ignore bad email addresses and do not raise email delivery errors.

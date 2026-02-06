7cb9d53cee4d0e946e92b551a4c4717324568a47
Kasper Timm Hansen <hey@kaspth.com>
2026-02-06 14:23:43 +0100

    one-shot render? capability

--- app/models/peak/rendering.rb
+++ app/models/peak/rendering.rb
@@ -2,6 +2,7 @@ module Peak::Rendering
   def self.included(klass) = klass.class_attribute(:to_partial_path)
 
   def render_in(view, &)
-    view.render(partial: to_partial_path, object: self, &)
+    view.render(partial: to_partial_path, object: self, &) if render?
   end
+  def render? = true
 end

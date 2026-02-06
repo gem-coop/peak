5314afa2e1fa078232594501268023c1e3cf7908
Kasper Timm Hansen <hey@kaspth.com>
2026-02-06 14:13:26 +0100

    add Peak::Error that we can style later; simplify Peak::Rendering for now

--- app/models/peak/rendering.rb
+++ app/models/peak/rendering.rb
@@ -1,5 +1,5 @@
 module Peak::Rendering
-  def self.included(klass) = klass.include(ActiveModel::API)
+  def self.included(klass) = klass.class_attribute(:to_partial_path)
 
   def render_in(view, &)
     view.render(partial: to_partial_path, object: self, &)

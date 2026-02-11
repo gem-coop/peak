4a8be25ef6d26bcf7dab906734a611e5a91cd970
Kasper Timm Hansen <hey@kaspth.com>
2026-02-11 18:49:47 +0100

    add bin/relay to copy gems + gem push and profile fixes

--- app/controllers/namespaces/gems_controller.rb
+++ app/controllers/namespaces/gems_controller.rb
@@ -33,7 +33,7 @@ class Namespaces::GemsController < ApplicationController
 
   private
     def authenticate_index_by_user_push_key
-      @user = User::PushKey.active.find_by!(token: request.authorization).user
+      @user = User::PushKey.active.find_by!(token: request.authorization.delete_prefix("Bearer ")).user
       @index = @user.namespaces.named(params[:namespace]).external_index
     rescue ActiveRecord::RecordNotFound
       if @user

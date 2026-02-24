e48f52340ace17594f57be1457df1d3e8ab32c4a
Kasper Timm Hansen <hey@kaspth.com>
2026-02-24 19:07:59 +0100

    create platforms in dev too

---
+++ db/seeds/data/platforms.rb
@@ -0,0 +1,10 @@
+Peak::Platform.ids_from [
+  "aarch64-linux",
+  "aarch64-linux-musl",
+  "arm-linux",
+  "arm-linux-gnueabihf",
+  "arm64-darwin",
+  "arm64-linux",
+  "universal-darwin",
+  "universal-darwin-22",
+]
--- test/models/peak/platform_test.rb
+++ test/models/peak/platform_test.rb
@@ -2,17 +2,6 @@ require "test_helper"
 
 class Peak::PlatformTest < ActiveSupport::TestCase
   test "parsing" do
-    Peak::Platform.ids_from [
-      "aarch64-linux",
-      "aarch64-linux-musl",
-      "arm-linux",
-      "arm-linux-gnueabihf",
-      "arm64-darwin",
-      "arm64-linux",
-      "universal-darwin",
-      "universal-darwin-22",
-    ]
-
     assert_platform "",          "ruby",   ""
     assert_platform "universal", "darwin", ""
     assert_platform "universal", "darwin", "22"

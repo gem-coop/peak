bcd596d9bfddd67febada72faf5cc0bdaa5e3bc2
Kasper Timm Hansen <hey@kaspth.com>
2026-02-24 21:40:55 +0100

    expand email_verifications to 24 hours

--- app/models/user/email_verification.rb
+++ app/models/user/email_verification.rb
@@ -1,6 +1,6 @@
 class User::EmailVerification < ActiveRecord::AssociatedObject
   def self.find_signed!(id) = super(id, purpose: attribute_name)
-  def signed_id(expires_in: 6.hours) = record.signed_id(purpose: self.class.attribute_name, expires_in:)
+  def signed_id(expires_in: 24.hours) = record.signed_id(purpose: self.class.attribute_name, expires_in:)
 
   def verify
     user.update! email_address_verified_at: Time.current unless user.email_address_verified_at?

77aef155f17c1e41493de76afbe44260a2d63fc7
Kasper Timm Hansen <hey@kaspth.com>
2026-02-25 10:24:42 +0100

    remove sign in cruft, we're not there yet

--- app/controllers/user/email_verifications_controller.rb
+++ app/controllers/user/email_verifications_controller.rb
@@ -2,14 +2,7 @@ class User::EmailVerificationsController < ApplicationController
   def show
     @verification = User::EmailVerification.find_signed!(params[:id])
     @verification.verify
-    start_new_session_for @verification.user
 
-    # TODO: Set up a user sign in route
     render Peak::Status("Your email's been verified and you're signed in!")
-  end
-
-  def start_new_session_for(user)
-    reset_session
-    cookies.encrypted[:user_id] = user
   end
 end

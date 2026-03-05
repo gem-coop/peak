3dc45c81e4266f2afd95e9e799aa4039ef029ea8
André Arko <andre@arko.net>
2026-03-04 18:59:11 -0800

    oops that was deprecated in Rails 7.1

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,5 @@
 Sidekiq.configure_server do |config|
   config.on(:startup) do
-    ActiveRecord::Base.clear_active_connections!
+    ActiveRecord::Base.connection_handler.clear_active_connections!
   end
 end

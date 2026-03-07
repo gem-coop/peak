d39c72337fa2b1cdac53f747efb4ec815241a02f
André Arko <andre@arko.net>
2026-03-06 22:44:40 -0800

    format email verified into a bool for Avo

--- app/avo/resources/user.rb
+++ app/avo/resources/user.rb
@@ -12,13 +12,17 @@ class Avo::Resources::User < Avo::BaseResource
     field :accesses, as: :has_many
     field :namespaces, as: :has_many, through: :accesses
     field :push_key, as: :has_one
-    field :email_address_verified_at, as: :boolean, name: "Email verified"
+    field :email_address_verified_at, as: :boolean, name: "Email verified" do
+      !!record.email_address_verified_at
+    end
   end
 
   def display_fields
     field :id, as: :id
     field :name, as: :text
     field :email_address, as: :text
-    field :email_address_verified_at, as: :boolean, name: "Email verified"
+    field :email_address_verified_at, as: :boolean, name: "Email verified" do
+      !!record.email_address_verified_at
+    end
   end
 end

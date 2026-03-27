3f110cfe12dd784004bd94d4bcb9ed97de989a39
Kasper Timm Hansen <hey@kaspth.com>
2026-03-27 11:57:59 +0100

    cut needless indirection; use reset_session to prevent session fixations

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -13,30 +13,24 @@ module Authentication
 
   private
     def require_authentication
-      resume_session || request_authentication
+      resume_session || redirect_to_sign_in
     end
+    def redirect_to_sign_in = redirect_to(sign_in_url(redirect_url: request.url))
 
     def resume_session
-      Current.session ||= find_session_by_cookie
-    end
-
-    def find_session_by_cookie
-      User::Session.find_by(id: session[:user_session_id]) if session[:user_session_id]
-    end
-
-    def request_authentication
-      redirect_to sign_in_url(redirect_url: request.url)
-    end
-
-    def start_new_session_for(user)
-      user.sessions.create!(user_agent: request.user_agent, ip_address: request.remote_ip).tap do |session_record|
-        Current.session = session_record
-        session[:user_session_id] = session_record.id
+      if id = session[:user_session_id]
+        Current.session ||= User::Session.find_by(id:)
       end
     end
 
+    def start_new_session_for(user)
+      reset_session
+      Current.session = user.sessions.create!(ip_address: request.remote_ip, user_agent: request.user_agent)
+      session[:user_session_id] = Current.session.id
+    end
+
     def terminate_session
-      Current.session.destroy
-      session.delete(:user_session_id)
+      reset_session
+      Current.session&.destroy
     end
 end

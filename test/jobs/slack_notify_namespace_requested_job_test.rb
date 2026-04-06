e9b02f1f8003464940bb056c8ad61ea0ef23e2b5
Kasper Timm Hansen <hey@kaspth.com>
2026-04-06 17:08:00 +0200

    Extract `Route` module to access routes consistently, including Avo ones. (#166)
    In case I comment out the Avo gem our whole suite fails with errors
    like:
    
    ```
    Error:
    CooldownControllerTest#test_bundle_install_endpoints_work_across_hourly_updates:
    NameError: uninitialized constant Avo::Engine
        app/jobs/slack_notify_namespace_requested_job.rb:4:in '<class:SlackNotifyNamespaceRequestedJob>'
        app/jobs/slack_notify_namespace_requested_job.rb:1:in '<main>'
        app/models/namespace.rb:14:in 'block in <class:Namespace>'
    ```

--- test/jobs/slack_notify_namespace_requested_job_test.rb
+++ test/jobs/slack_notify_namespace_requested_job_test.rb
@@ -2,16 +2,14 @@ require "test_helper"
 
 class SlackNotifyNamespaceRequestedJobTest < ActiveJob::TestCase
   test "it notifies slack" do
-    Slack.webhook_url = "https://slack.test/webhook"
+    Slack.with webhook_url: "https://slack.test/webhook" do
+      text = +"Namespace @slacktest requested"
+      text << "\nhttp://www.example.com/avo/resources/namespaces/@slacktest" if Peak.avo?
+      webhook = stub_request(:post, Slack.webhook_url).with(body: {"payload" => JSON.dump({text:})})
 
-    msg = "Namespace @slacktest requested\nhttp://www.example.com/avo/resources/namespaces/@slacktest"
-    webhook = stub_request(:post, "https://slack.test/webhook").
-      with(body: {"payload" => JSON.dump({"text": msg})})
-
-    ns = Namespace.build(name: "@slacktest")
-    SlackNotifyNamespaceRequestedJob.new.perform(ns)
-    assert_requested(webhook)
-  ensure
-    Slack.webhook_url = nil
+      namespace = Namespace.build(name: "@slacktest")
+      SlackNotifyNamespaceRequestedJob.perform_now(namespace)
+      assert_requested(webhook)
+    end
   end
 end

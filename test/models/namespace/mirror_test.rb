a1a2ba5e9a65d02b76ed2c27c11fc3b30483d1d1
André Arko <andre@arko.net>
2026-04-07 23:22:56 -0700

    refactor into named mirror scenarios

--- test/models/namespace/mirror_test.rb
+++ test/models/namespace/mirror_test.rb
@@ -11,28 +11,33 @@ class Namespace::MirrorTest < ActiveSupport::TestCase
   test "import versions" do
     assert_equal [], mirror_rake_versions
 
-    stub_request(:get, "https://rubygems.org/versions").to_return(body: file_fixture("versions").read.lines[0...-4].join)
-    stub_request(:get, "https://rubygems.org/info/rake").to_return(body: file_fixture("info/rake").read.lines[0...-3].join)
+    rake_12_versions = ["0.4.10", "0.4.11", "0.4.12", "0.4.13", "0.4.14", "0.4.15", "0.4.8", "0.4.9", "0.5.0", "0.5.3", "0.5.4", "0.6.0", "0.6.2", "0.7.0", "0.7.1", "0.7.2", "0.7.3", "0.8.0", "0.8.1", "0.8.2", "0.8.3", "0.8.4", "0.8.5", "0.8.6", "0.8.7", "0.9.0", "0.9.0.beta.0", "0.9.0.beta.1", "0.9.0.beta.2", "0.9.0.beta.4", "0.9.0.beta.5", "0.9.1", "0.9.2", "0.9.2.2", "0.9.3", "0.9.3.beta.1", "0.9.3.beta.2", "0.9.3.beta.3", "0.9.3.beta.4", "0.9.4", "0.9.5", "0.9.6", "10.0.0", "10.0.0.beta.1", "10.0.0.beta.2", "10.0.1", "10.0.2", "10.0.3", "10.0.4", "10.1.0", "10.1.0.beta.1", "10.1.0.beta.2", "10.1.0.beta.3", "10.1.1", "10.2.0", "10.2.1", "10.2.2", "10.3.0", "10.3.1", "10.3.2", "10.4.0", "10.4.1", "10.4.2", "10.5.0", "11.0.1", "11.1.0", "11.1.1", "11.1.2", "11.2.0", "11.2.2", "11.3.0", "12.0.0", "12.0.0.beta1", "12.1.0", "12.2.0", "12.2.1", "12.3.0", "12.3.1", "12.3.2", "12.3.3"]
+    rake_13_versions = rake_12_versions + ["13.0.0", "13.0.0.pre.1", "13.0.1", "13.0.2", "13.0.3", "13.0.4", "13.0.5", "13.0.6", "13.1.0", "13.2.0", "13.2.1", "13.3.0"]
+
+    stub_rubygems("rake-12")
     perform_import
+    assert_equal rake_12_versions, mirror_rake_versions
 
-    rake_versions = ["0.4.10", "0.4.11", "0.4.12", "0.4.13", "0.4.14", "0.4.15", "0.4.8", "0.4.9", "0.5.0", "0.5.3", "0.5.4", "0.6.0", "0.6.2", "0.7.0", "0.7.1", "0.7.2", "0.7.3", "0.8.0", "0.8.1", "0.8.2", "0.8.3", "0.8.4", "0.8.5", "0.8.6", "0.8.7", "0.9.0", "0.9.0.beta.0", "0.9.0.beta.1", "0.9.0.beta.2", "0.9.0.beta.4", "0.9.0.beta.5", "0.9.1", "0.9.2", "0.9.2.2", "0.9.3", "0.9.3.beta.1", "0.9.3.beta.2", "0.9.3.beta.3", "0.9.3.beta.4", "0.9.4", "0.9.5", "0.9.6", "10.0.0", "10.0.0.beta.1", "10.0.0.beta.2", "10.0.1", "10.0.2", "10.0.3", "10.0.4", "10.1.0", "10.1.0.beta.1", "10.1.0.beta.2", "10.1.0.beta.3", "10.1.1", "10.2.0", "10.2.1", "10.2.2", "10.3.0", "10.3.1", "10.3.2", "10.4.0", "10.4.1", "10.4.2", "10.5.0", "11.0.1", "11.1.0", "11.1.1", "11.1.2", "11.2.0", "11.2.2", "11.3.0", "12.0.0", "12.0.0.beta1", "12.1.0", "12.2.0", "12.2.1", "12.3.0", "12.3.1", "12.3.2", "12.3.3", "13.0.0", "13.0.0.pre.1", "13.0.1", "13.0.2", "13.0.3", "13.0.4", "13.0.5", "13.0.6", "13.1.0", "13.2.0", "13.2.1", "13.3.0"]
+    assert_equal mirror_fixture("rake-12", "versions").read, mirrors.public.upstream.versions
+    assert_equal mirror_fixture("rake-12", "info/rake").read, mirrors.public.upstream.info("rake")
 
-    assert_equal rake_versions[0..-3], mirror_rake_versions
-
-    stub_request(:get, "https://rubygems.org/versions").to_return(body: file_fixture("versions").read)
-    stub_request(:get, "https://rubygems.org/info/rake").to_return(body: file_fixture("info/rake").read)
-
+    stub_rubygems("rake-13")
     perform_import
+    assert_equal rake_13_versions, mirror_rake_versions
 
-    # get the whole file if we don't have a byte offset
-    assert_equal file_fixture("versions").read, mirrors.public.upstream.versions
-    assert_equal file_fixture("info/rake").read, mirrors.public.upstream.info("rake")
+    assert_equal mirror_fixture("rake-13", "versions").read, mirrors.public.upstream.versions
+    assert_equal mirror_fixture("rake-13", "info/rake").read, mirrors.public.upstream.info("rake")
 
-    assert_equal rake_versions, mirror_rake_versions
   end
 
-# test "import versions after compaction"
-# test "import versions when a yank was hidden by compaction"
+test "import versions after compaction, respecting yanks" do
+
+  # gem gains new compact row
+  # gem with previous compact row grows
+  # gem previously published has no row
+  # gem version previouly in compact row is now gone
+
+end
 
 private
 
@@ -46,5 +51,19 @@ private
   def mirror_rake_versions
     rake = namespaces.public.external_index.gems.find_by(name: "rake")
     rake ? rake.versions.pluck(:ref).sort : []
+  end
+
+  def stub_rubygems(scenario)
+    dir = Rails.root.join("test/fixtures/files/mirror").join(scenario)
+    stub_request(:get, "https://rubygems.org/versions").
+      to_return(body: dir.join("versions").read)
+    dir.glob("info/*").each do |file|
+      stub_request(:get, "https://rubygems.org/info/#{file.basename}").
+        to_return(body: file.read)
+    end
+  end
+
+  def mirror_fixture(scenario, file)
+    Rails.root.join("test/fixtures/files/mirror").join(scenario).join(file)
   end
 end

eedd5c697a907df5f922d003f0b867d3172fa72f
André Arko <andre@arko.net>
2026-04-19 15:49:10 +0900

    fix migrations to actually run, use db:migrate:reset to rebuild schema

--- db/migrate/20260216135907_add_created_by_to_namespace_gem_versions.rb
+++ db/migrate/20260216135907_add_created_by_to_namespace_gem_versions.rb
@@ -1,8 +1,17 @@
 class AddCreatedByToNamespaceGemVersions < ActiveRecord::Migration[8.1]
+  class User < ActiveRecord::Base; end
+
   def change
     add_reference :namespace_gem_versions, :created_by
 
-    up_only { Namespace::Gem::Version.update_all created_by_id: Peak.system_user.id }
+    up_only do
+      User.reset_column_information
+      # create Peak system user with our no-callback User class
+      User.create_with(name: "gem.coop system").find_or_create_by!(email_address: "support@gem.coop")
+      # update the existing versions
+      Namespace::Gem::Version.update_all created_by_id: Peak.system_user.id
+    end
+
     change_column_null :namespace_gem_versions, :created_by_id, false
   end
 end

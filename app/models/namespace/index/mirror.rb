b4c31128ba2fbcb909d46fef8c9bfa5b55afb199
André Arko <andre@arko.net>
2026-04-21 13:12:22 +0900

    skip rebuild if no versions were inserted

--- app/models/namespace/index/mirror.rb
+++ app/models/namespace/index/mirror.rb
@@ -84,7 +84,7 @@ class Namespace::Index::Mirror < ApplicationRecord
     gem.versions.where(id: imported_ids).trigger_precompile_later_bulk
 
     gem.versions.where.not(ref: vset).destroy_all
-    gem.info.rebuild(mirror_checksum: hash)
+    gem.info.rebuild(mirror_checksum: hash) if imported_ids.any?
   end
   performs :import_line
 end

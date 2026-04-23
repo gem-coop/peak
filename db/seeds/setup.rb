f7e831a53e7758b634bb68eeb085b4b293391e82
Kasper Timm Hansen <hey@kaspth.com>
2026-04-23 22:33:01 +0200

    Fix `db:seed` reruns (#193)
    Appending `dependent: :destroy` to the has_one index definitions seems
    off to me, it's possible we just not want them anymore. I'll have to
    look into that separately.
    
    Use `create` instead of `build` so we can Oaken's uniqueness checks
    working for us automatically. I'd like to look into what else we can do
    in Oaken here automatically.
    
    Closes #192

--- db/seeds/setup.rb
+++ db/seeds/setup.rb
@@ -21,8 +21,11 @@ accesses.proxy *Namespace::Access.roles.keys
 def accesses.create(label = nil, unique_by: [:namespace, :user], **) = super
 
 versions.with do
+  # TODO: Figure out why we need `versions.` here for it to work.
+  def versions.create(label = nil, unique_by: [:gem, :ref], **) = super
+
   def upload(gem, ref:, **)
-    build(gem:, ref:, **).tap { _1.process fixture_upload("#{gem.name}/#{_1.filename}") }
+    create(gem:, ref:, **).tap { _1.process fixture_upload("#{gem.name}/#{_1.filename}") }
   end
 
   def by(gem, ref:) = type.find_by!(gem:, ref:)

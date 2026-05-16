525176c6089a0e25dc83620455fa799f86f56416
Kasper Timm Hansen <hey@kaspth.com>
2026-05-16 20:04:54 +0200

    Manifest: append seems to rank higher than compact, feels like it should go above

--- app/models/namespace/index/manifest.rb
+++ app/models/namespace/index/manifest.rb
@@ -1,14 +1,14 @@
 class Namespace::Index::Manifest < ApplicationRecord
   belongs_to :author, polymorphic: true, touch: true
 
-  performs def compact
-    update! compacted_at: compacted_at = Time.current,
-      contents: "#{compacted_at.iso8601}\n---\n#{composed_envelopes}"
-  end
-
   performs def append(versions)
     self.contents <<= Array(versions).map { _1.envelope ref_stamp: _1.ref }.join
     save!
+  end
+
+  performs def compact
+    update! compacted_at: compacted_at = Time.current,
+      contents: "#{compacted_at.iso8601}\n---\n#{composed_envelopes}"
   end
 
   private

60e5e1c0fac34e03944d989bbcdee0432d06515a
Kasper Timm Hansen <hey@kaspth.com>
2026-05-17 00:06:07 +0200

    Manifest: fixes after noticing deploy (#219)
    - Manifest's didn't have the frontmatter in there, so now we initiate a
    compact to get that started.
    - I'm worried about OOM building up the contents, so now we generate the
    work in batches and build up a long string instead of an array of
    strings that's joined. So hopefully a little better.
    - Our local bin/push also needs the namespace name, not an index locator
    ala `@kaspth/dev`.

--- app/models/namespace/index/manifest.rb
+++ app/models/namespace/index/manifest.rb
@@ -1,5 +1,8 @@
 class Namespace::Index::Manifest < ApplicationRecord
   belongs_to :author, polymorphic: true, touch: true
+  delegate :gems, :versions, to: :author
+
+  after_create :compact_later
 
   performs def append(versions)
     self.contents <<= Array(versions).map { _1.envelope ref_stamp: _1.ref }.join
@@ -8,12 +11,12 @@ class Namespace::Index::Manifest < ApplicationRecord
 
   performs def compact
     update! compacted_at: compacted_at = Time.current,
-      contents: "#{compacted_at.iso8601}\n---\n#{composed_envelopes}"
+      contents: "#{compacted_at.iso8601}\n---\n#{computed_contents}"
   end
 
   private
-    def composed_envelopes
+    def computed_contents
       # TODO: later, use https://github.com/bensheldon/activerecord-has_some_of_many
-      author.gems.filter_map { author.versions.latest_for(_1.name)&.envelope }.join
+      gems.find_each.pluck(:name).inject(+"") { |str, name| str << versions.latest_for(name)&.envelope.to_s }
     end
 end

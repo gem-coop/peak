d7462af0e48c0647679d71eb0a782a0a5072034f
André Arko <andre@arko.net>
2026-05-18 01:21:45 -0700

    return echo json even if csrf fails

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < Public::BaseController
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
+  skip_before_action :verify_authenticity_token, only: [:echo]
   def echo
     render plain: JSON.pretty_generate({
       csrf: {

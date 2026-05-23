f9d064170d36aa16c544f5bbd7716bbf9a9b0667
Kasper Timm Hansen <hey@kaspth.com>
2026-05-23 17:29:52 +0200

    drop Namespace::Gem::Info and Namespace::Index#{versions_contents,las… (#231)
    …t_compacted_at}

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_26_081929) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_16_223802) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -180,11 +180,9 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_26_081929) do
   create_table "namespace_indexes", force: :cascade do |t|
     t.string "access", default: "public", null: false
     t.datetime "created_at", null: false
-    t.datetime "last_compacted_at", null: false
     t.integer "namespace_id", null: false
     t.string "slug", null: false
     t.datetime "updated_at", null: false
-    t.text "versions_contents", default: "---\n", null: false
     t.index ["namespace_id", "slug"], name: "namespace_index_uniqueness", unique: true
     t.index ["namespace_id"], name: "index_namespace_indexes_on_namespace_id"
   end

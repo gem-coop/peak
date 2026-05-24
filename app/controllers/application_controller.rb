b8d6d95340be0ec799e36d3e67d515855cbde610
André Arko <andre@arko.net>
2026-05-24 03:35:02 -0400

    remove the (unsafe) debugging echo action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,24 +4,4 @@ class ApplicationController < Public::BaseController
 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-
-  skip_before_action :verify_authenticity_token, only: [:echo]
-  def echo
-    render plain: JSON.pretty_generate({
-      csrf: {
-        valid_origin: valid_request_origin?,
-        valid_token: any_authenticity_token_valid?,
-        form_param: form_authenticity_token,
-        header: request.x_csrf_token
-      },
-      session: session.to_h,
-      request: {
-        base_url: request.base_url,
-        origin: request.origin,
-        host: request.host
-      },
-      params: params.permit!.to_h,
-      headers: request.headers.select { |k, v| !k.include?(".") }.sort_by(&:first).to_h
-    })
-  end
 end

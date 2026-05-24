953852ca0c1d002695993a81303c509fd61e4f3b
Kasper Timm Hansen <hey@kaspth.com>
2026-05-24 17:02:29 +0200

    Peak::Gem.version split on first dash before digit-sequence + period
    fixes https://github.com/gem-coop/peak/issues/234

--- test/models/peak/gem_test.rb
+++ test/models/peak/gem_test.rb
@@ -3,5 +3,6 @@ require "test_helper"
 class Peak::GemTest < ActiveSupport::TestCase
   test "version splitting with extra dash" do
     assert_equal ["active_job-performs", "1.0.0"], Peak::Gem.version("active_job-performs-1.0.0.gem")
+    assert_equal ["active_job-performs", "1.0.0-arm64-darwin-23"], Peak::Gem.version("active_job-performs-1.0.0-arm64-darwin-23.gem")
   end
 end

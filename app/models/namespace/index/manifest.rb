e01e2ec3bead1d27accf78ddbc1560ab3a6bf1da
Kasper Timm Hansen <hey@kaspth.com>
2026-06-02 13:37:52 +0200

    fix Manifest#find failing on job execution after create (#246)
    it's the ol' case of jobs executing before the transaction commits so
    the job Active Record database connection can't see the data.
    
    Active Job supposedly defers jobs to after commit now, and I've
    previously checked that we have that config enabled, but we'll have to
    investigate further.

--- app/models/namespace/index/manifest.rb
+++ app/models/namespace/index/manifest.rb
@@ -2,7 +2,8 @@ class Namespace::Index::Manifest < ApplicationRecord
   belongs_to :author, polymorphic: true, touch: true
   delegate :gems, :versions, to: :author
 
-  after_create :compact_later
+  # TODO: Look into why Active Job execution can't find Manifest, despite supposedly deferring jobs to after transaction commit now.
+  after_create_commit :compact_later
 
   performs def append(versions)
     self.contents <<= Array(versions).map { _1.envelope ref_stamp: _1.ref }.join

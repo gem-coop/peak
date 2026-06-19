7946fa74cfe5ed0c31c707162a23ff9161c74608
Samuel Giddins <segiddins@segiddins.me>
2026-06-19 16:38:35 -0500

    Extract ExpiringToken concern from User::PushKey

--- app/models/user/push_key.rb
+++ app/models/user/push_key.rb
@@ -1,15 +1,12 @@
 class User::PushKey < ApplicationRecord
+  include ExpiringToken
+
   belongs_to :user
   attribute :expires_at, default: -> { 24.hours.from_now }
 
-  scope :active, -> { where(expires_at: Time.current..).order(expires_at: :desc) }
-  scope :expired, -> { where(expires_at: ..Time.current) }
   performs :destroy
 
   has_secure_token
-
-  def active? = !expired?
-  def expired? = expires_at.past?
 
   def sign_in_mailer
     Mailer.with(push_key: self).sign_in

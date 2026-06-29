90f27c953ecd101515bd2d8da925d2ab92ffe77b
Kasper Timm Hansen <hey@kaspth.com>
2026-06-29 10:40:15 +0200

    implement compaction in pure Ruby
    if we can get compaction working correctly in pure Ruby,
    it's possible we could port this to Rust and run compaction as Fastly Edge Compute.
    
    basically there's two formats here: a non-compacted gem line like `oaken 1.0.0 abcdef`
    and a compacted one with multiple versions separated by a comma `oaken 1.0.0,1.0.1 defghi`.
    
    we can read a file from the end and up by line to gather new versions, if we hit a line with a ",",
    we know we've previously been compacted. since lines are written when gems are pushed, the first encounter
    of a gem is the latest correct checksum we append to previous lines if any.
    
    if we have previous compactions it's a matter of appending to the right line, e.g.
    `oaken 1.0.0 abcdef` -> `oaken 1.0.0,1.0.1 defghi`. and if it's not there, we have to
    insert it alphabetically. e.g. if the only other gem is activesupport, oaken would go after it and so on.

--- app/models/namespace/index/manifest.rb
+++ app/models/namespace/index/manifest.rb
@@ -17,6 +17,42 @@ class Namespace::Index::Manifest < ApplicationRecord
 
   private
     def computed_contents
-      versions.distinct_on_gem_name.latest_first.as_byline.inject(+"") { _1 << _2.envelope }
+      # versions.distinct_on_gem_name.latest_first.as_byline.inject(+"") { _1 << _2.envelope }
+      lines = contents.sub(/\A.*\n---\n/, "").lines
+
+      appenders = {}
+      previously_compacted = false
+
+      lines.reverse.map! do |line|
+        previously_compacted = true if line.include?(",")
+
+        name = line.byteslice 0, line.index(" ")
+
+        if previously_compacted
+          if append = appenders.delete(name)
+            line.bytesplice line.rindex(" ").., append
+          else
+            line
+          end
+        else
+          if appenders[name]
+            version = line.byteslice name.size.succ...line.rindex(" ")
+            appenders[name].prepend(",").prepend(version)
+          else
+            appenders.store name, line.byteslice(name.size.succ..)
+          end
+
+          ""
+        end
+      end
+
+      if previously_compacted
+        lines.reverse!
+      else
+        puts appenders
+        lines = appenders.keys.sort.map { |name| "#{name} #{appenders[name]}" }
+      end
+
+      lines.join
     end
 end
--- test/models/namespace/index/manifest_test.rb
+++ test/models/namespace/index/manifest_test.rb
@@ -1,15 +1,12 @@
 require "test_helper"
 
 class Namespace::Index::ManifestTest < ActiveSupport::TestCase
-  test "index compaction" do
+  test "compact" do
     manifest = namespaces.public.default_index.manifest
     assert_changes(-> { manifest.compacted_at }) { manifest.compact }
 
     lines = manifest.contents.lines
-    assert_equal lines.size, lines.uniq.size
-
-    lines = lines.drop(2) # Drop metadata lines
-    assert_equal lines, lines.sort_by { _1.split(" ").first }
+    assert_contents_shape lines
 
     assert_includes lines, "oaken 0.9.1,1.0.0 33837e788e120ccc642d3c4dc7e0f532\n"
     assert_includes lines, "activesupport 8.1.1 14576e848f997e40e8b92864a3e84cfb\n"
@@ -17,7 +14,22 @@ class Namespace::Index::ManifestTest < ActiveSupport::TestCase
     assert_includes lines, manifest.versions.latest_for(:actionview).envelope
   end
 
-  test "cooldown compaction" do
+  test "compact when previously compacted" do
+    manifest = namespaces.public.default_index.manifest.tap(&:compact)
+
+    manifest.update! contents: manifest.contents << "actionview 7.0.0 abcdef\noaken 0.5.0 abcdef\n"
+    manifest.compact
+
+    lines = manifest.contents.lines
+    assert_contents_shape lines
+
+    assert_includes lines, "activesupport 8.1.1 14576e848f997e40e8b92864a3e84cfb\n"
+    assert_includes lines, "actionview 7.0.0,7.0.1,7.0.9,7.0.10,7.1.6,7.2.3,8.0.4,8.1.0,8.1.0.rc1,8.1.1,8.1.2 2bbe194954a8e9129fb50e76a80b7579\n"
+    assert_includes lines, "oaken 0.5.0,0.9.1,1.0.0 33837e788e120ccc642d3c4dc7e0f532\n"
+    assert_includes lines, manifest.versions.latest_for(:actionview).envelope
+  end
+
+  test "compact cooldown" do
     cooldown = namespaces.public.default_index.cooldowns.create
     travel 2.days + 1.second
 
@@ -29,14 +41,19 @@ class Namespace::Index::ManifestTest < ActiveSupport::TestCase
     assert_changes(-> { manifest.compacted_at }) { manifest.compact }
 
     lines = manifest.contents.lines
-    assert_equal lines.size, lines.uniq.size
-
-    lines = lines.drop(2) # Drop metadata lines
-    assert_equal lines, lines.sort_by { _1.split(" ").first }
+    assert_contents_shape lines
 
     assert_includes lines, "oaken 0.9.1 cbc0bdc1633e8ee470c322abc91340b0\n"
     refute_includes lines, "activesupport 8.1.1 14576e848f997e40e8b92864a3e84cfb\n"
     assert_includes lines, "actionview 7.0.9,7.0.10,7.1.6,7.2.3 fc355f85935a75c158b042ed679f73cd\n"
     assert_includes lines, cooldown.versions.latest_for(:actionview).envelope
   end
+
+  private
+    def assert_contents_shape(lines)
+      assert_equal lines.size, lines.uniq.size
+
+      lines = lines.slice(2..) # Drop metadata lines
+      assert_equal lines, lines.sort_by { _1.split(" ", 2).first }
+    end
 end

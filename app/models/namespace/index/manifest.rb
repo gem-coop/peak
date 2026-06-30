ffc12b77f267cf2e5984260e30f90f72eea5b4d8
Josef Šimánek <josef.simanek@gmail.com>
2026-06-30 15:59:06 +0200

    Lock manifest appends to avoid losing concurrent entries

--- app/models/namespace/index/manifest.rb
+++ app/models/namespace/index/manifest.rb
@@ -6,8 +6,8 @@ class Namespace::Index::Manifest < ApplicationRecord
   after_create_commit :compact_later
 
   performs def append(versions)
-    self.contents <<= Array(versions).map { _1.envelope ref_stamp: _1.ref }.join
-    save!
+    addition = Array(versions).map { _1.envelope ref_stamp: _1.ref }.join
+    with_lock { update! contents: "#{contents}#{addition}" }
   end
 
   performs def compact

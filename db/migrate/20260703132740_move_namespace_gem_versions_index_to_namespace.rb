e12f252500d08e965c4d926153804f80d3c4258b
Kasper Timm Hansen <hey@kaspth.com>
2026-07-11 17:43:54 +0200

    fix gem version index we want

--- db/migrate/20260703132740_move_namespace_gem_versions_index_to_namespace.rb
+++ db/migrate/20260703132740_move_namespace_gem_versions_index_to_namespace.rb
@@ -2,7 +2,7 @@ class MoveNamespaceGemVersionsIndexToNamespace < ActiveRecord::Migration[8.1]
   def change
     safety_assured {
       remove_index :namespace_gem_versions, [:gem_id, :ref], name: "index_namepace_gem_versions_uniqueness", unique: true
-      add_index :namespace_gem_versions, [:gem_id, :index_id, :ref], name: "index_namepace_gem_versions_uniqueness", unique: true
+      add_index :namespace_gem_versions, [:gem_id, :index_id, :platform_id, :ref], name: "index_namepace_gem_versions_uniqueness", unique: true
     }
   end
 end

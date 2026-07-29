gem-coop/peak

commit a9093d347e2fa806abe44b618b84f97177d46cc5

move gems to namespace instead of within indexes (#334)

app/models/namespace/index.rb
@@ -1,5 +1,8 @@
 class Namespace::Index < ApplicationRecord
   belongs_to :namespace
+  has_many :gems, through: :namespace
+  has_many :versions, class_name: "Namespace::Gem::Version", dependent: :destroy
+
   has_many :cooldowns, dependent: :destroy
 
   concerning :Manifested do
@@ -8,9 +11,6 @@ class Namespace::Index < ApplicationRecord
       delegate :compact, :compact_later, :append, :append_later, :contents, to: :manifest
     end
   end
-
-  has_many :gems, dependent: :destroy
-  has_many :versions, through: :gems, class_name: "Namespace::Gem::Version"
 
   enum :access, %i[public private].index_by(&:itself), suffix: true
 
@@ -22,13 +22,15 @@ class Namespace::Index < ApplicationRecord
     find_by!(slug: slug.presence || :default)
   end
 
-  def process_version(version)
+  def version_from(name:, ref:)
+    versions.find_or_initialize_by(ref:, gem: gems.find_or_create_by!(name:))
+  end
+
+  performs def process_version(version)
     append version
     cooldowns.each { _1.project version }
   end
+  def version_uploaded(version) = process_version_later(version)
 
-  def routable
-    self unless default?
-  end
   def default? = slug == "default"
 end

app/controllers/namespaces/gems_controller.rb
@@ -6,7 +6,7 @@ class Namespaces::GemsController < Public::BaseController
 
   def create
     upload = Current.upload_from(request.body).validate
-    version = @index.gems.version_from name: upload.name, ref: upload.platform_ref
+    version = @index.version_from name: upload.name, ref: upload.platform_ref
 
     if version.persisted?
       render plain: "Upload skipped: #{version.package_name} already exists. ❌", status: :conflict

app/models/namespace.rb
@@ -8,6 +8,7 @@ class Namespace < ApplicationRecord
   has_many :owners, through: :ownerships, source: :user
 
   has_many :indexes, dependent: :destroy
+  has_many :gems, dependent: :destroy
   has_one_built :default_index, -> { public_access.where(slug: :default) }, class_name: "Index"
 
   scope :named_like, -> { _1.blank? ? none : where("name ILIKE ?", "%#{_1}%") }

app/models/namespace/gem.rb
@@ -1,8 +1,8 @@
 class Namespace::Gem < ApplicationRecord
   include SearchIndex::Indexed
 
-  belongs_to :index
-  belongs_to :namespace, default: -> { index.namespace }
+  belongs_to :namespace
+  self.ignored_columns = ["index_id"]
 
   has_many :referrants, class_name: "Version::Reference", foreign_key: :name, primary_key: :name
   has_many :versions, dependent: :destroy
@@ -15,15 +15,7 @@ class Namespace::Gem < ApplicationRecord
   def self.named(name) = find_by!(name:)
   def to_param = name
 
-  def self.version_from(name:, ref:)
-    find_or_create_by!(name:).versions.find_or_initialize_by(ref:)
-  end
-
-  def version_uploaded(version)
-    reindex_later
-    process_version_later(version)
-  end
-  performs def process_version(version) = index.process_version(version)
+  def version_uploaded(version) = reindex_later
 
   def namespaced_name
     "#{namespace.name}/#{name}"

app/models/namespace/gem/version.rb
@@ -1,6 +1,6 @@
 class Namespace::Gem::Version < ApplicationRecord
   belongs_to :gem
-  belongs_to :index, default: -> { gem.index }
+  belongs_to :index
   belongs_to :created_by, class_name: "User"
 
   has_many :linkings, dependent: :destroy
@@ -24,7 +24,7 @@ class Namespace::Gem::Version < ApplicationRecord
   scope :latest_last, -> { order(published_at: :asc, ref: :asc) }
   def self.latest = latest_last.last
 
-  def versions_upto_self = gem.versions.upto(self)
+  def versions_upto_self = index.versions.where(slice(:gem_id)).upto(self)
   scope :upto, -> { published_before(_1.published_at) }
 
   scope :for,    -> { joins(:gem).where(gem: {name: _1}) }
@@ -32,7 +32,7 @@ class Namespace::Gem::Version < ApplicationRecord
   scope :distinct_on_gem_name, -> { includes(:gem).distinct_on("namespace_gems.name").merge(Namespace::Gem.alphabetized) }
   def self.latest_for(name) = self.for(name).latest
 
-  scope :as_byline, -> { select(:id, :ref, :summary, :published_at, :gem_id, :created_by_id).includes(:gem, :created_by) }
+  scope :as_byline, -> { select(:id, :ref, :summary, :published_at, :index_id, :gem_id, :created_by_id).includes(:gem, :created_by) }
 
   belongs_to :platform, class_name: "Peak::Platform"
 
@@ -56,6 +56,7 @@ class Namespace::Gem::Version < ApplicationRecord
   def process(...)
     consume(...)
     gem.version_uploaded self
+    index.version_uploaded self
   end
 
   def consume(upload, **)
@@ -71,7 +72,7 @@ class Namespace::Gem::Version < ApplicationRecord
     # function = Arel.sql("concat_ws(' ', string_agg(ref::text, ','), md5(string_agg(line::text, '')))")
     # "#{gem.name} #{versions_upto_self.pick(function)}\n"
 
-    versions = versions_upto_self
+    versions_upto_self => versions
     "#{gem.name} #{ref_stamp || versions.refs.join(",")} #{versions.checksum}\n"
   end
 

config/routes.rb
@@ -50,7 +50,7 @@ Rails.application.routes.draw do
     get "/:namespace", to: "namespaces/profiles#show", as: :namespace
   end
 
-  resolve("Namespace::Gem") { route_for(:namespace_gem, _1.namespace, _1, index: _1.index.routable) }
+  resolve("Namespace::Gem") { route_for(:namespace_gem, _1.namespace, _1, index: nil) }
 
   get "terms/acceptances/new",  to: "peak/terms/acceptances#new",    as: :new_terms_acceptances
   post "terms/:id/acceptances", to: "peak/terms/acceptances#create", as: :terms_acceptances

db/migrate/20260703131553_change_column_null_index_id_on_namespace_gems.rb
@@ -1,0 +1,5 @@
+class ChangeColumnNullIndexIdOnNamespaceGems < ActiveRecord::Migration[8.1]
+  def change
+    change_column_null :namespace_gems, :index_id, true
+  end
+end

db/migrate/20260703132434_move_namespace_gems_index_to_namespace.rb
@@ -1,0 +1,9 @@
+class MoveNamespaceGemsIndexToNamespace < ActiveRecord::Migration[8.1]
+  def change
+    safety_assured {
+      remove_index :namespace_gems, :index_id, name: "index_namespace_gems_on_index_id"
+      remove_index :namespace_gems, [:index_id, :name], name: "index_namepace_gems_uniqueness", unique: true
+      add_index    :namespace_gems, [:namespace_id, :name], name: "index_namepace_gems_uniqueness", unique: true
+    }
+  end
+end

commit 42b7d388b62141aa8dfa1d61fcd7bc5c39991441

Create gem in proper namespace.
- fixes initial gem publishing
- seems broken by a9093d3
- covered by new tests

app/models/namespace/index.rb
@@ -23,7 +23,7 @@ class Namespace::Index < ApplicationRecord
   end
 
   def version_from(name:, ref:)
-    versions.find_or_initialize_by(ref:, gem: gems.find_or_create_by!(name:))
+    versions.find_or_initialize_by(ref:, gem: namespace.gems.find_or_create_by!(name:))
   end
 
   performs def process_version(version)

test/controllers/namespaces/gems_controller_test.rb
@@ -81,6 +81,24 @@ class Namespaces::GemsControllerTest < ActionDispatch::IntegrationTest
     assert_equal users.owner, first.created_by
   end
 
+  test "push first version of a new gem" do
+    package = gem_package_from(name: "fresh")
+
+    assert_increments Namespace::Gem, Namespace::Gem::Version do
+      post namespace_gem_push_url(namespace:), env: { "RAW_POST_DATA" => package, authorization: "Bearer #{token}" }
+    end
+    assert_response :success
+    assert_match "fresh-1.0.0.gem uploaded 🎉", response.body
+
+    gem = namespace.gems.find_by!(name: "fresh")
+    version = versions.by gem, ref: "1.0.0"
+
+    assert_equal namespace.stable_index, version.index
+    assert_equal users.plain, version.created_by
+    assert version.package.attached?
+    assert_equal package, version.package.download
+  end
+
   test "push platform" do
     package = file_fixture "peak/peak-0.1.0-arm-linux.gem"
 

test/models/namespace/index_test.rb
@@ -5,4 +5,16 @@ class Namespace::IndexTest < ActiveSupport::TestCase
     index = namespaces.gemcoop.stable_index.tap(&:destroy)
     refute index.destroyed?
   end
+
+  test "version from a new gem" do
+    index = namespaces.gemcoop.stable_index
+
+    assert_increments Namespace::Gem do
+      version = index.version_from(name: "new-gem", ref: "1.0.0")
+
+      refute version.persisted?
+      assert_equal index, version.index
+      assert_equal index.namespace.gems.find_by!(name: "new-gem"), version.gem
+    end
+  end
 end

test/models/peak/gem/upload_test.rb
@@ -19,6 +19,19 @@ class Peak::Gem::UploadTest < ActiveSupport::TestCase
     assert_invalid_upload gem_package_from(executables: ["peak\nforged"])
   end
 
+  test "validation with jar requirements" do
+    requirements = [
+      "jar org.bouncycastle:bcprov-jdk18on, 1.80",
+      "jar org.bouncycastle:bcpkix-jdk18on, 1.80",
+      "jar org.bouncycastle:bctls-jdk18on, 1.80",
+      "jar org.bouncycastle:bcutil-jdk18on, 1.80"
+    ]
+    upload = peak_upload_from gem_package_from(platform: "java", requirements:)
+
+    assert upload.validate
+    assert_equal requirements, upload.spec.requirements
+  end
+
   test "ref format" do
     refute valid_ref?("1.0\n9")
     refute valid_ref?("1.0 0")
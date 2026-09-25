03bbdf2946ae9b711ff8fae5704d1ddfe4960533
André Arko <andre@arko.net>
2026-09-25 14:08:44 -0700

    group class and instance stuff together

--- app/models/namespace/gem/version.rb
+++ app/models/namespace/gem/version.rb
@@ -2,6 +2,7 @@ class Namespace::Gem::Version < ApplicationRecord
   belongs_to :gem
   belongs_to :index, class_name: "Namespace::Index"
   belongs_to :created_by, class_name: "User"
+  belongs_to :platform, class_name: "Peak::Platform"
 
   has_many :linkings, dependent: :destroy
   has_many :links, through: :linkings
@@ -18,6 +19,9 @@ class Namespace::Gem::Version < ApplicationRecord
   end
   has_many :referrants, -> { where(ref: _1.ref) }, through: :gem, foreign_key: :ref, primary_key: :ref
 
+  has_object :metadata
+  has_one_attached :package
+
   scope :published_before, -> { where(published_at: .._1) }
   scope :published_after, -> { where(published_at: _1..) }
   scope :latest_first, -> { order(published_at: :desc, ref: :desc) }
@@ -33,25 +37,20 @@ class Namespace::Gem::Version < ApplicationRecord
   def self.latest_for(name) = self.for(name).latest
 
   scope :as_byline, -> { select(:id, :ref, :summary, :published_at, :index_id, :gem_id, :created_by_id).includes(:gem, :created_by) }
-
-  belongs_to :platform, class_name: "Peak::Platform"
-
   scope :missing_precompiles, -> { has_extensions.joins(:platform).merge(Peak::Platform.precompile_targeted) }
   scope :has_extensions, -> { where(has_extensions: true) }
   scope :pure, -> { joins(:platform).merge(Peak::Platform.pure) }
+
+  def self.checksum = Digest::MD5.hexdigest(lines)
+  def self.lines = latest_last.pluck(:line).join
+  def self.refs = latest_last.pluck(:ref)
 
   def to_param = filename
   def filename = "#{name}.gem"
   def name = "#{gem.name}-#{ref}"
   alias_method :package_name, :filename
 
-  has_object :metadata
-  has_one_attached :package
   attribute :published_at, default: -> { Time.current }
-
-  def self.checksum = Digest::MD5.hexdigest(lines)
-  def self.lines = latest_last.pluck(:line).join
-  def self.refs = latest_last.pluck(:ref)
 
   def process(...)
     consume(...)

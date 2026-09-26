e5d851dca4962704eb961d65542cd899f25d7290
André Arko <andre@arko.net>
2026-09-26 13:22:36 -0700

    use if_exists in migration (ht @kaspth)

--- db/migrate/20260923034104_remove_cooldown_versions_again.rb
+++ db/migrate/20260923034104_remove_cooldown_versions_again.rb
@@ -1,5 +1,5 @@
 class RemoveCooldownVersionsAgain < ActiveRecord::Migration[8.1]
   def change
-    drop_table :cooldown_versions if table_exists? :cooldown_versions
+    drop_table :cooldown_versions, if_exists: true
   end
 end

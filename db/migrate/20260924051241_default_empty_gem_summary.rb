3b79bea897477727d3e599f8f11c677f9fb76d79
André Arko <andre@arko.net>
2026-09-26 13:45:28 -0700

    make gem summary and drop migrations reversible

--- db/migrate/20260924051241_default_empty_gem_summary.rb
+++ db/migrate/20260924051241_default_empty_gem_summary.rb
@@ -1,5 +1,5 @@
 class DefaultEmptyGemSummary < ActiveRecord::Migration[8.1]
   def change
-    change_column_default :namespace_gem_versions, :summary, ""
+    change_column_default :namespace_gem_versions, :summary, to: "", from: nil
   end
 end
